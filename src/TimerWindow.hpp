8b670b68b665f7f80a9f7a1b0524de81fc004803
GrogDeFrog <george.knightly@gmail.com>
2024-07-17 08:44:25 -0700

    Attempt to fix window positioning

--- src/TimerWindow.hpp
+++ src/TimerWindow.hpp
@@ -1,15 +1,16 @@
 #ifndef __TIMER_WINDOW_HPP__
 #define __TIMER_WINDOW_HPP__
 
-enum
-{
-    ID_Hello = 1
-};
-
 class TimerWindow : public wxFrame
 {
 public:
     TimerWindow();
+
+    enum
+    {
+        ID_Hello = 1
+    };
+
 private:
     void OnHello(wxCommandEvent &event);
     void OnExit(wxCommandEvent &event);
